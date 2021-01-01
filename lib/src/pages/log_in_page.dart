fb4deda6a5a4e901e3bf87a538301051466c1e78
oOddaviidOo <ddaavviidd0004@gmail.com>
2021-01-02 00:06:12 +0100

    Añadidos botones de libreria para mejorar la interfaz
    Libreria flutter_signin_button. Falta añadir el código para el registro e inicio por correo

--- lib/src/pages/log_in_page.dart
+++ lib/src/pages/log_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:fontana/src/log_in_state.dart';
 import 'package:provider/provider.dart';
 
@@ -7,62 +8,52 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Consumer(
-          builder: (BuildContext context, LoginState value, Widget child){
-            if (value.isLoading()) {
-              return CircularProgressIndicator();
-            } else {
-              return child;
-            }
-          },
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text('Bienvenido '),
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        RaisedButton(
-                            child: Text('Inicia sesion anonima'),
-                            onPressed: () {
-                              Provider.of<LoginState>(context, listen: false)
-                                  .loginAnonymous();
-                            }),
-                        RaisedButton(
-                            child: Text('Inicia sesion con Google'),
-                            onPressed: () {
-                              Provider.of<LoginState>(context, listen: false)
-                                  .loginGoogle();
-                            }),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        RaisedButton(
-                            child: Text('Registrate'),
-                            onPressed: () {
-                              Provider.of<LoginState>(context, listen: false)
-                                  .loginGoogle();
-                            }),
-                        RaisedButton(
-                            child: Text('Inicia Sesión'),
-                            onPressed: () {
-                              Provider.of<LoginState>(context, listen: false)
-                                  .loginGoogle();
-                            }),
-                      ],
-                    )
-                  ],
-                )
-              ],
+        body: Center(
+      child: Consumer(
+        builder: (BuildContext context, LoginState value, Widget child) {
+          if (value.isLoading()) {
+            return CircularProgressIndicator();
+          } else {
+            return child;
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SignInButtonBuilder(
+              text: 'Inicio de sesión anónimo',
+              icon: Icons.person,
+              onPressed: () { Provider.of<LoginState>(context, listen: false)
+                                  .loginAnonymous();},
+              backgroundColor: Colors.grey[600],
+              width: 220.0,
             ),
-          ),
+            Divider(),
+            SignInButtonBuilder(
+              text: 'Registro con email',
+              icon: Icons.email,
+              onPressed: () {},
+              backgroundColor: Colors.blueGrey,
+              width: 220.0,
+            ),
+            Divider(),
+            SignInButtonBuilder(
+              text: 'Inicio de sesión con email',
+              icon: Icons.email,
+              onPressed: () {},
+              backgroundColor: Colors.blueGrey[700],
+              width: 220.0,
+            ),
+            Divider(),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                Provider.of<LoginState>(context, listen: false).loginGoogle();
+              },
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
