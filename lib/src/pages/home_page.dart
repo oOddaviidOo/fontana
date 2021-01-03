4167ac5619762787a0256b6b7c66d7919956c92b
oOddaviidOo <ddaavviidd0004@gmail.com>
2021-01-03 04:01:10 +0100

    Eliminada variable que controlaba si se mostraba o no el Appbar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _showAppBar = true;
+  //bool _showAppBar = true;
   User user;
   UserService _userService;
   Completer<GoogleMapController> _controller = Completer();
